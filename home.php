fc4c2743867b883084d30fca81111d63c19909e7
Karlypm29 <karlypm29@gmail.com>
2023-07-22 02:16:55 -0600

    Imagenes

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Whycux</title>
+    <title>SHOP</title>
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
     <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
